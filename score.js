8a82c84ad432b8306eb08ae7095ab799e668810e
Ryan Gleason <rjg5404@psu.edu>
2019-04-10 06:41:41 -0400

    Added support for multiple dimensions

--- score.js
+++ score.js
@@ -1,14 +1,13 @@
 const outputs = [];
 
-const k = 3;
-
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 	// Ran every time a balls drops into a bucket
 	outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
+// Initiate test, generate report
 function runAnalysis() {
-	const testSetSize = 10;
+	const testSetSize = 100;
 	const [testSet, trainingSet] = splitDataSet(outputs, testSetSize);
 
 	let numberCorrect = 0;
@@ -19,14 +18,34 @@ function runAnalysis() {
 		}
 	}
 
-	console.log("Accuracy: ", numberCorrect / testSetSize);
+	_.range(1, 20).forEach(k => {
+		const accuracy = _.chain(testSet)
+			.filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+			.size()
+			.divide(testSetSize)
+			.value();
+
+		console.log("For k of ", k, " Accuracy: ", accuracy);
+	});
 }
 
+// Calculate distance using any number of factors
 function distance(pointA, pointB) {
-	return Math.abs(pointA - pointB);
+	// pointA = [300, .5, 16], pointB = ...
+
+	return (
+		_.chain(pointA)
+			.zip(pointB)
+			.map(([a, b]) => (a - b) ** 2)
+			.sum()
+			.value() ** 0.5
+	);
 }
 
-function knn(data, point) {
+// Implement knn function
+// Calculate distance from test to trained data
+// Find the most likely buckets
+function knn(data, point, k) {
 	return _.chain(data)
 		.map(row => [distance(row[0], point), row[3]])
 		.sortBy(row => row[0])
@@ -40,7 +59,8 @@ function knn(data, point) {
 		.value();
 }
 
-//Splitting data into Training and Test to find ideal K
+// Splitting data into Training and Test to find ideal K
+// Shuffle data so its not just first half
 function splitDataSet(data, testCount) {
 	const shuffled = _.shuffle(data);
 
